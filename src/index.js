b2299582c0a399fc4c9d4e9a0c432886102a2559
veramsteixeira <129678252+veramsteixeira@users.noreply.github.com>
2023-04-29 15:13:20 +0100

    forecast coordinates

--- src/index.js
+++ src/index.js
@@ -38,7 +38,12 @@ function formatDate(timestamp) {
   return `${weekday}, ${month} ${day}, ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+let mainDate = document.querySelector("#date");
+let currentDate = new Date();
+mainDate.innerHTML = formatDate(currentDate);
+
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
   let forecastHTML = `<div class="row">`;
@@ -66,9 +71,12 @@ function displayForecast() {
   console.log(forecastHTML);
 }
 
-let mainDate = document.querySelector("#date");
-let currentDate = new Date();
-mainDate.innerHTML = formatDate(currentDate);
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "c15c4d36993e58eb1cbcc771ca3eccb4";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -92,6 +100,8 @@ function displayTemperature(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -154,4 +164,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
-displayForecast();
