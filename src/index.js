441d7275ba7505ec80748e4f345a5506cf2ece98
veramsteixeira <129678252+veramsteixeira@users.noreply.github.com>
2023-04-29 14:51:57 +0100

    Added forecast function

--- src/index.js
+++ src/index.js
@@ -38,6 +38,34 @@ function formatDate(timestamp) {
   return `${weekday}, ${month} ${day}, ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
 let mainDate = document.querySelector("#date");
 let currentDate = new Date();
 mainDate.innerHTML = formatDate(currentDate);
@@ -126,3 +154,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
+displayForecast();
